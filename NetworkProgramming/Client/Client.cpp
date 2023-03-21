bd48134e144556daf793808698446775f2ca4e81
jeonggyun <jeonggyun0630@gmail.com>
2023-03-21 23:40:45 +0900

    Non-blocking Socket

--- NetworkProgramming/Client/Client.cpp
+++ NetworkProgramming/Client/Client.cpp
@@ -23,73 +23,77 @@ int main()
     if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) return 0;
 
     SOCKET clntSock;
-    clntSock = ::socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    clntSock = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (clntSock == INVALID_SOCKET)
     {
         ErrHandling();
         return 0;
     }
 
+    u_long lMode = 1;
+    ::ioctlsocket(clntSock, FIONBIO, &lMode);
+
     SOCKADDR_IN servAdr;
     ::memset(&servAdr, 0, sizeof(servAdr));
     servAdr.sin_family = AF_INET;
-    //servAdr.sin_addr.s_addr = ::inet_addr("127.0.0.1");
     ::InetPtonA(AF_INET, "127.0.0.1", &servAdr.sin_addr);
-    servAdr.sin_port = htons(7777);
-
-    // UDP는 연결의 개념이 아니다. ( Connected UDP, UnConnected UDP)
-    // But, Connected UDP
-    // 연결이 된 것이 아니라 등록해놓는 개념.
-    if (SOCKET_ERROR == ::connect(clntSock, reinterpret_cast<SOCKADDR*>(&servAdr), sizeof(servAdr)))
-    {
-        ErrHandling();
-        return 0;
-    }
-
-    std::cout << "Connected To Server!!" << std::endl;
+    servAdr.sin_port = htons(9999);
 
     while (true)
     {
-        char sendBuffer[100] = "Hello World";
-        
-        // UnConnected UDP
-        //int sendSize = ::sendto(clntSock, sendBuffer, sizeof(sendBuffer), 0, reinterpret_cast<SOCKADDR*>(&servAdr), sizeof(servAdr));
-        
-        // Connected UDP
-        int sendSize = ::send(clntSock, sendBuffer, sizeof(sendBuffer), 0);
-
-        if (sendSize == SOCKET_ERROR)
+        if (SOCKET_ERROR == ::connect(clntSock, reinterpret_cast<SOCKADDR*>(&servAdr), sizeof(servAdr)))
         {
+            if (::WSAGetLastError() == WSAEWOULDBLOCK) continue;
+
+            if (::WSAGetLastError() == WSAEISCONN || ::WSAGetLastError() == WSAEALREADY) break;
+
             ErrHandling();
             return 0;
         }
+    }
 
-        std::cout << "SendData!! Size: " << sizeof(sendBuffer) << std::endl;
-        this_thread::sleep_for(1s);
+    std::cout << "Connection Success" << std::endl;
 
-        char recvBuffer[1000];
-        SOCKADDR_IN recvAdr;
-        memset(&recvAdr, 0, sizeof(recvAdr));
-        int recvAdrSize = sizeof(servAdr);
-        
-        // UnConnected UDP
-        //int recvSize = ::recvfrom(clntSock, recvBuffer, sizeof(recvBuffer), 0, reinterpret_cast<SOCKADDR*>(&recvAdr), &recvAdrSize);
-
-        // Connected UDP
-        int recvSize = ::recv(clntSock, recvBuffer, sizeof(recvBuffer), 0);
-
-        if (recvSize == SOCKET_ERROR)
+    // Send
+    while (true)
+    {
+        char sndBuf[100] = "Hello World";
+        int sendSize = ::send(clntSock, sndBuf, sizeof(sndBuf), 0);
+        if (sendSize == SOCKET_ERROR)
         {
-            ErrHandling();
-            return -1;
+            if (::WSAGetLastError() == WSAEWOULDBLOCK) continue;
         }
-        else if (recvSize == 0)
+        else if (sendSize == 0)
         {
-            std::cout << "Client Exit" << std::endl;
+            std::cout << "Client Connection Terminate" << std::endl;
+        }
+
+        std::cout << "Send Len: " << sendSize << std::endl;
+           
+        while (true)
+        {
+            char recvBuf[1000];
+            int recvSize = ::recv(clntSock, recvBuf, sizeof(recvBuf), 0);
+            if (recvSize == SOCKET_ERROR)
+            {
+                if (::WSAGetLastError() == WSAEWOULDBLOCK)
+                    continue;
+
+                ErrHandling();
+                return 0;
+            }
+            else if (recvSize == 0)
+            {
+                std::cout << "Client Terminate" << std::endl;
+                break;
+            }
+
+            std::cout << "Recv Len: " << recvSize << std::endl;
+            std::cout << "Recv Data: " << recvBuf << std::endl;
             break;
         }
     }
-
+   
     ::closesocket(clntSock);
     
     // WinSock terminate
--- NetworkProgramming/Server/Server.cpp
+++ NetworkProgramming/Server/Server.cpp
@@ -7,9 +7,11 @@
 using namespace std;
 #pragma comment(lib, "ws2_32.lib")
 
-void ErrHandling()
+int ErrHandling()
 {
-    std::cout << WSAGetLastError() << std::endl;
+    int errCode = ::WSAGetLastError();
+    std::cout << errCode << std::endl;
+    return errCode;
 }
 
 int main()
@@ -17,67 +19,97 @@ int main()
     WSAData wsaData;
     if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) return 0;
 
-    SOCKET servSock = ::socket(AF_INET, SOCK_DGRAM, 0);
-    if (servSock == INVALID_SOCKET)
+    SOCKET listenSock = ::socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSock == INVALID_SOCKET)
     {
         ErrHandling();
         return 0;
     }
 
-    // SOCKET OPTION
-     
-    // SO_KEEPALIVE -> 주기적으로 연결 상태 확인 여부 (TCP Only)
-    // 상대방이 연결을 끊는다면?? -> TCP Layer에서 끊어진 연결 감진    
-    bool enable = true;
-    ::setsockopt(servSock, SOL_SOCKET, SO_KEEPALIVE, reinterpret_cast<char*>(&enable), sizeof(enable));
-
-    // SO_LINGER = 지연
-    // onoff = 0이면 closesocket()이 바로 리턴, 아니면 linger초 만큼 대기 (default 0)
-    // linger : 대기시간
-    LINGER linger;
-    linger.l_onoff = 1;
-    linger.l_linger = 5;
-    ::setsockopt(servSock, SOL_SOCKET, SO_LINGER, reinterpret_cast<char*>(&linger), sizeof(linger));
-
-    // Half-Close
-    // SD_SEND : send 금지
-    // SD_RECV : recv 금지
-    // SD_BOTH : 둘다 막음
-    //::shutdown(servSock, SD_SEND);
-
-    // SO_SNDBUF = 송신 버퍼 크기
-    // SO_RCVBUF = 수신 버퍼 크기
-    
-    int sendBufSize;
-    int optionLen = sizeof(sendBufSize);
-    ::getsockopt(servSock, SOL_SOCKET, SO_SNDBUF, reinterpret_cast<char*>(&sendBufSize), &optionLen);
-
-    std::cout << "Send Buf Size: " << sendBufSize << std::endl;
-
-    int recvBufSize;
-    optionLen = sizeof(recvBufSize);
-    ::getsockopt(servSock, SOL_SOCKET, SO_RCVBUF, reinterpret_cast<char*>(&recvBufSize), &optionLen);
-
-    std::cout << "Recv Buf Size: " << recvBufSize << std::endl;
-
-    // SO_REUSEADDR
-    // IP 주소 및 PORT 재 사용
+    u_long lMode = 1; // Set non-blocking mode socket
+    if (SOCKET_ERROR == ::ioctlsocket(listenSock, FIONBIO, &lMode))
     {
-        bool enable = true;
-        ::setsockopt(servSock, SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<char*>(&enable), sizeof(enable));
+        ErrHandling();
+        return 0;
     }
 
-    // IPPROTO_TCP
-    // TCP_NODELAY = Nagle Algorithm 
-    // 장점: 작은 패킷이 불필요하게 많이 생성되는 일 방지
-    // 단점: 반응 시간 손해
+    SOCKADDR_IN servAdr;
+    ::memset(&servAdr, 0, sizeof(servAdr));
+    servAdr.sin_family = AF_INET;
+    servAdr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+    servAdr.sin_port = ::htons(9999);
+
+    if (SOCKET_ERROR == ::bind(listenSock, reinterpret_cast<SOCKADDR*>(&servAdr), sizeof(servAdr)))
     {
-        bool enable = true;
-        ::setsockopt(servSock, IPPROTO_TCP, TCP_NODELAY, reinterpret_cast<char*>(&enable), sizeof(enable));
+        ErrHandling();
+        return 0;
+    }
+
+    if (SOCKET_ERROR == ::listen(listenSock, SOMAXCONN))
+    {
+        ErrHandling();
+        return 0;
+    }
+
+    SOCKADDR_IN clntAdr;
+    int clntAdrSz = sizeof(clntAdr);
+
+    // Accept
+    while (true)
+    {
+        SOCKET clntSock = ::accept(listenSock, reinterpret_cast<SOCKADDR*>(&clntAdr), &clntAdrSz);
+        if (clntSock == INVALID_SOCKET)
+        {
+            if (::WSAGetLastError() == WSAEWOULDBLOCK)
+                continue;
+            else
+                break;
+        }
+
+        std::cout << "Client Connected" << std::endl;
+
+        // Recv
+        while (true)
+        {
+            char recvBuf[1000];
+            int recvSize = ::recv(clntSock, recvBuf, sizeof(recvBuf), 0);
+            if (recvSize == SOCKET_ERROR)
+            {
+                if (::WSAGetLastError() == WSAEWOULDBLOCK)
+                    continue;
+                
+                ErrHandling();
+                return 0;
+            }
+            else if (recvSize == 0)
+            {
+                std::cout << "Client Terminate" << std::endl;
+                break;
+            } 
+            
+            std::cout << "Recv Len: " << recvSize << std::endl;
+            std::cout << "Recv Data: " << recvBuf << std::endl;
+            
+            // Send
+            while (true)
+            {
+                int sendSize = ::send(clntSock, recvBuf, recvSize, 0);
+                if (sendSize == SOCKET_ERROR)
+                {
+                    if (::WSAGetLastError() == WSAEWOULDBLOCK) continue;
+                }
+                else if (sendSize == 0)
+                {
+                    std::cout << "Client Connection Terminate" << std::endl;
+                }
+                
+                break;
+            }
+        }
     }
 
 
-    ::closesocket(servSock);
+    ::closesocket(listenSock);
     ::WSACleanup();
     return 0;
 }
